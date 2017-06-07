ce1c357187d28fb9e0d5df7e75a63bed530aa64b
yog prakash <yprakash@tekion.com>
2017-06-07 15:20:30 +0530

    removed second readme file

--- src/app-config.js
+++
@@ -1,6 +0,0 @@
-
-export default {
-	GOOGLE_API_KEY: 'AIzaSyBLo3ba_-TDaSICqNeTFKCmfBSukyiziL8',
-	TEKION_BASE_URL: process.env.TEKION_API_URL,
-	TEKION_TDP_API_URL: process.env.TEKION_TDP_API_URL
-};
